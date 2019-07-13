0555c336778b1018cc892594527adffe98a0a94c
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2019-07-13 20:44:41 +0800

    added spinning wheel

---
+++ rhack/Assets/getRotations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getRotations : MonoBehaviour
+{   public GameObject temp_object_1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(temp_object_1.GetComponent<Transform>().rotation.y);
+    }
+}
